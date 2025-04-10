ccf0c26249107f7b073e3591b01f7e0e2132a026
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-10 21:44:30 +0900

    init

---
+++ src/main/java/com/sharipov/topuch/TopuchApplication.java
@@ -0,0 +1,13 @@
+package com.sharipov.topuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharipov/topuch/TopuchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharipov.topuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
