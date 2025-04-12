05d4a9b84af59ea619b3037f34bd31f4b59ad32a
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-12 13:47:56 +0900

    fix: fixed bug with incorrect data type

---
+++ src/main/java/com/sharipov/topuch/entity/Report.java
@@ -0,0 +1,28 @@
+package com.sharipov.topuch.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "reports")
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long reportId;
+
+    private LocalDateTime reportedAt;
+
+    @Enumerated(EnumType.STRING)
+    private Abuse abuseType;
+    private String  message;
+
+    @OneToOne
+    @JoinColumn(name = "post_id")
+    private Post postId;
+
+    @ManyToOne
+    @JoinColumn(name = "profile_id")
+    private Profile profileId;
+
+}
