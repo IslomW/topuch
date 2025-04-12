fd48e05de4248ae8e655c2488b8b4ea1617f91ff
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-12 13:52:28 +0900

    fix: fixed bug with data type

--- src/main/java/com/sharipov/topuch/entity/Profile.java
+++ src/main/java/com/sharipov/topuch/entity/Profile.java
@@ -1,14 +1,23 @@
 package com.sharipov.topuch.entity;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "profiles")
 public class Profile {
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long profileId;
     private String first_name;
     private String last_name;
-    private String signup_ts;
+    private LocalDateTime createdAt;
     private Long phoneNumber;
     private String email;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
     private Address address;
 }
