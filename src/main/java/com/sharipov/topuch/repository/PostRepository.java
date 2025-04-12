3df6fbb3be52daf4eaf469bb40aff8bd0f188a14
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-12 13:57:08 +0900

    add(address.png, er_diagram.png):add new files

--- src/main/java/com/sharipov/topuch/repository/PostRepository.java
+++ src/main/java/com/sharipov/topuch/repository/PostRepository.java
@@ -1,2 +1,9 @@
-package com.sharipov.topuch.repository;public class PostRepository {
+package com.sharipov.topuch.repository;
+
+import com.sharipov.topuch.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
