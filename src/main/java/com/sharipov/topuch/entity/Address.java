84700f68476a6c12e3ac5dfb4a0a5e26a9493812
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-12 13:49:12 +0900

    fix: fixed bug with data type

--- src/main/java/com/sharipov/topuch/entity/Address.java
+++ src/main/java/com/sharipov/topuch/entity/Address.java
@@ -1,6 +1,15 @@
 package com.sharipov.topuch.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long addressId;
     private String country_code;
     private String state;
     private String city;
