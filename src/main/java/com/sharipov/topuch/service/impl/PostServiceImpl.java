58a7875bcb774c00730a2440d683e29e796d8b9d
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-13 14:46:21 +0900

    chore(PostServiceImp): add realizations service

--- src/main/java/com/sharipov/topuch/service/impl/PostServiceImpl.java
+++ src/main/java/com/sharipov/topuch/service/impl/PostServiceImpl.java
@@ -1,11 +1,24 @@
-package com.sharipov.topuch.service;
+package com.sharipov.topuch.service.impl;
 
 
+import com.sharipov.topuch.entity.Post;
+import com.sharipov.topuch.exception.PostNotFound;
+import com.sharipov.topuch.exception.PostUnchangedException;
 import com.sharipov.topuch.repository.PostRepository;
+import com.sharipov.topuch.service.PostService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import java.util.stream.Collectors;
+
 @Service
-public class PostServiceImpl {
+public class PostServiceImpl implements PostService {
+    private final Logger log = LoggerFactory.getLogger(PostServiceImpl.class);
 
     private final PostRepository repository;
 
@@ -14,5 +27,68 @@ public class PostServiceImpl {
     }
 
 
+    @Override
+    public List<Post> getAllPosts() {
+        return repository.findAll();
+    }
 
+    @Override
+    public Optional<Post> getPostById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Post createPost(Post post) {
+        Post result = repository.save(post);
+        return result;
+    }
+
+    @Override
+    public Post updatePost(Post post) {
+        Post exist = getPostById(post.getPostId())
+                .orElseThrow(() -> new PostNotFound("Post with Id:" + post.getPostId() + " not found"));
+
+        if (exist.equals(post)) {
+            throw new PostUnchangedException("Post with Id:" + post.getPostId() + " has not been changed");
+        }
+
+        return repository.save(post);
+    }
+
+    @Override
+    public void deletePostById(Long id) {
+        Post exist = getPostById(id)
+                .orElseThrow(() -> new PostNotFound("Post with Id:" + id + " not found"));
+
+        repository.deleteById(id);
+
+    }
+
+    @Override
+    public void deleteOldPost() {
+        LocalDateTime sevenDaysAgo = LocalDateTime.now().minusDays(7);
+
+        List<Long> oldPostsIds = repository.findAllIdsByCreateAtBefore(sevenDaysAgo);
+        oldPostsIds.forEach(id ->  log.info("Deleting post withId: {}", id));
+
+        repository.deleteAllOlderThan(sevenDaysAgo);
+
+
+//        List<Post> posts = getAllPosts();
+//
+//        if (posts.isEmpty()){
+//            throw  new PostNotFound("Posts not found");
+//        }
+//
+//        List<Post> oldPosts = posts.stream()
+//                .filter(post -> post.getCreated_at().isBefore(sevenDaysAgo))
+//                .collect(Collectors.toList());
+//
+//        if (!oldPosts.isEmpty()){
+//            for (Post post : oldPosts){
+//                deletePostById(post.getPostId());
+//            }
+//        }
+
+    }
 }
