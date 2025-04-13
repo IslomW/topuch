28a6103d76b72daa8364a819df5ec57e65e60f88
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-13 14:47:11 +0900

    chore(PostRepository): add PostRepository interface

--- src/main/java/com/sharipov/topuch/repository/PostRepository.java
+++ src/main/java/com/sharipov/topuch/repository/PostRepository.java
@@ -1,9 +1,25 @@
 package com.sharipov.topuch.repository;
 
 import com.sharipov.topuch.entity.Post;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Query("select p.postId from Post p where p.created_at < :date")
+    List<Long> findAllIdsByCreateAtBefore(@Param("date")LocalDateTime date);
+
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Post p WHERE p.created_at < :date")
+    void deleteAllOlderThan(@Param("date") LocalDateTime date);
 }
