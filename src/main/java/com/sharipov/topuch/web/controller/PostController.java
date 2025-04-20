e9f9fdd91d4462440bb6ca67521dec063bdd73aa
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-20 12:41:13 +0900

    fix(controller) fixed bugs

--- src/main/java/com/sharipov/topuch/web/controller/PostController.java
+++ src/main/java/com/sharipov/topuch/web/controller/PostController.java
@@ -1,34 +1,70 @@
 package com.sharipov.topuch.web.controller;
 
 
+import com.sharipov.topuch.application.converter.PostMapper;
+import com.sharipov.topuch.application.dto.request.PostRequestDTO;
+import com.sharipov.topuch.application.dto.request.ProfileRequestDTO;
+import com.sharipov.topuch.application.dto.response.PostResponseDTO;
 import com.sharipov.topuch.domain.entity.Post;
 import com.sharipov.topuch.domain.service.PostService;
-import com.sharipov.topuch.application.dto.PostDTO;
+import org.springframework.boot.convert.PeriodUnit;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping("api/post")
+@RequestMapping("api/posts")
 public class PostController {
 
     private final PostService postService;
+    private final PostMapper postMapper;
 
-    public PostController(PostService postService) {
+    public PostController(PostService postService, PostMapper postMapper) {
         this.postService = postService;
+        this.postMapper = postMapper;
     }
-
-//    @GetMapping
-//    public ResponseEntity<List<PostDTO>> getAllPosts(){
-//
-//
-//    }
 
     @GetMapping
-    public Post getPostById(Long id) {
-        return new Post();
+    public ResponseEntity<List<PostResponseDTO>> getAllPosts(){
+        List<Post> posts = postService.getAllPosts();
+
+        return ResponseEntity.ok(postMapper.toDtoList(posts));
+
     }
+
+    @GetMapping("{id}")
+    public ResponseEntity<PostResponseDTO> getPostById(@PathVariable Long id) {
+        Post post = postService.getPostById(id);
+        return ResponseEntity.ok(postMapper.toDto(post));
+    }
+
+    @PostMapping
+    public ResponseEntity<PostResponseDTO> createPost(@RequestBody PostRequestDTO requestDTO){
+        Post post = postMapper.toEntity(requestDTO);
+        Post savedPost = postService.createPost(post);
+        return ResponseEntity.status(HttpStatus.CREATED).body(postMapper.toDto(savedPost));
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<PostResponseDTO> updatePost(@PathVariable Long id,
+            @RequestBody PostRequestDTO postRequestDTO){
+
+        Post post = postService.updatePost(id, postMapper.toEntity(postRequestDTO));
+
+        return ResponseEntity.ok(postMapper.toDto(post));
+    }
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id){
+        postService.deletePostById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
--- src/main/java/com/sharipov/topuch/web/controller/ProfileController.java
+++ src/main/java/com/sharipov/topuch/web/controller/ProfileController.java
@@ -1,36 +1,73 @@
 package com.sharipov.topuch.web.controller;
 
+import com.sharipov.topuch.application.converter.ProfileMapper;
+import com.sharipov.topuch.application.dto.request.ProfileRequestDTO;
+import com.sharipov.topuch.application.dto.response.ProfileResponseDTO;
 import com.sharipov.topuch.domain.entity.Profile;
+import com.sharipov.topuch.domain.service.ProfileService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
 @RestController
-@RequestMapping("api/")
+@RequestMapping("api/users")
 public class ProfileController {
 
+    private final ProfileService profileService;
 
-    @GetMapping("users")
-    public Profile getAllUsers() {
-        Profile profile = new Profile();
-        return profile;
+    private final ProfileMapper profileMapper;
+
+    private final Logger log = LoggerFactory.getLogger(ProfileController.class);
+
+    public ProfileController(ProfileService profileService, ProfileMapper profileMapper) {
+        this.profileService = profileService;
+        this.profileMapper = profileMapper;
     }
 
 
-    @GetMapping("user/{id}")
-    public Profile getUserById(@PathVariable Long id) {
-        Profile profile = new Profile();
-        return profile;
-    }
-
-    @PostMapping("user")
-    public void createUser(@RequestBody Profile profile) {
+        @GetMapping
+    public ResponseEntity<List<ProfileResponseDTO>> getAllUsers() {
+        List<Profile> profiles = profileService.getAllProfile();
+        return ResponseEntity.ok(profileMapper.toDtoList(profiles));
     }
 
 
-//    @GetMapping
-//    public Profile getUser(){
-//
-//        return profile;
-//
-//    }
+    @GetMapping("{id}")
+    public ResponseEntity<ProfileResponseDTO> getUserById(@PathVariable Long id) {
+       Profile profile = profileService.getProfileById(id);
+        return ResponseEntity.ok(profileMapper.toDto(profile)) ;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProfileResponseDTO> createUser(@RequestBody ProfileRequestDTO profile) {
+        Profile result = profileService.createProfile(profileMapper.toEntity(profile));
+        return ResponseEntity.status(HttpStatus.CREATED).body(profileMapper.toDto(result));
+    }
+
+
+    @PutMapping("{id}")
+    public ResponseEntity<ProfileResponseDTO> updateUser(@PathVariable Long id,
+            @RequestBody ProfileRequestDTO profile){
+        Profile update =  profileService.updateProfile(id, profileMapper.toEntity(profile));
+        return ResponseEntity.ok(profileMapper.toDto(update));
+    }
+
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteUserById(@PathVariable Long id){
+        profileService.deleteProfileById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
+
+
 
 }
