8f44c1a416460340ee6e6e99cb3e32522542a886
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-27 23:29:41 +0900

    chore(PostUnchangedException): remove unused exception class

--- src/main/java/com/sharipov/topuch/domain/exception/PostUnchangedException.java
+++
@@ -1,8 +0,0 @@
-package com.sharipov.topuch.domain.exception;
-
-public class PostUnchangedException extends RuntimeException{
-
-    public PostUnchangedException(Long id) {
-        super("Post with Id:" + id + " has not been changed");
-    }
-}
