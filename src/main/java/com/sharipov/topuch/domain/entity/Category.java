2803dc8f8eb96d44a2910bccc631ef2e96ba888f
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-27 23:35:32 +0900

    fix(Category): fix Category parentId to Long parentId

--- src/main/java/com/sharipov/topuch/domain/entity/Category.java
+++ src/main/java/com/sharipov/topuch/domain/entity/Category.java
@@ -1,2 +1,41 @@
-package com.sharipov.topuch.domain.entity;public class Category {
+package com.sharipov.topuch.domain.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long categoryId;
+    private String name;
+
+    private Long parentId;
+
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Long parentId) {
+        this.parentId = parentId;
+    }
 }
