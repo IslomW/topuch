c0b45bebdeb299376d64329e80442836d75d9b42
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-27 23:34:41 +0900

    feat(CategoryRepository): add repository

--- src/main/java/com/sharipov/topuch/domain/repository/CategoryRepository.java
+++ src/main/java/com/sharipov/topuch/domain/repository/CategoryRepository.java
@@ -1,2 +1,17 @@
-package com.sharipov.topuch.domain.repository;public interface CategoryRepository {
+package com.sharipov.topuch.domain.repository;
+
+import com.sharipov.topuch.domain.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    @Query("select c from Category c where c.parent is null")
+    List<Category> findParentCategories();
+
+    @Query("select c from Category c where c.parent.id = :parentId")
+    List<Category> findSubcategoriesByParentId(@Param("parentId") Long parentId);
 }
