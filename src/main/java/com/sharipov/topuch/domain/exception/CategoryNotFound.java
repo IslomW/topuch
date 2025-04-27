c416cddce95f4355d08fd7502173f5de00241be7
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-27 23:36:43 +0900

    feat(CategoryNotFound): add exception

--- src/main/java/com/sharipov/topuch/domain/exception/CategoryNotFound.java
+++ src/main/java/com/sharipov/topuch/domain/exception/CategoryNotFound.java
@@ -1,2 +1,8 @@
-package com.sharipov.topuch.domain.exception;public class CategoryNotFound {
+package com.sharipov.topuch.domain.exception;
+
+public class CategoryNotFound extends RuntimeException{
+
+    public CategoryNotFound(Long id) {
+        super("Post with Id:" + id + " not found");
+    }
 }
