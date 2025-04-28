6ff70f0c37a211921b75b38a0a19fbe3b2594a82
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-28 23:27:31 +0900

    feat(ProductDocumentRepository): add Elasticsearch repository

--- src/main/java/com/sharipov/topuch/domain/repository/ProductDocumentRepository.java
+++ src/main/java/com/sharipov/topuch/domain/repository/ProductDocumentRepository.java
@@ -1,2 +1,9 @@
-package com.sharipov.topuch.domain.repository;public interface ProductDocumentRepository {
+package com.sharipov.topuch.domain.repository;
+
+import com.sharipov.topuch.domain.document.PostDocument;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ProductDocumentRepository extends ElasticsearchRepository<PostDocument, String> {
+
+
 }
