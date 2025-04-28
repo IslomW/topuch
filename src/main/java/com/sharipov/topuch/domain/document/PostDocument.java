5f8e159a43cc323b233e82e1c8d3a27fe8d3bcb1
IslomW <119986336+IslomW@users.noreply.github.com>
2025-04-28 23:26:30 +0900

    feat(PostDocument): add Elasticsearch document

--- src/main/java/com/sharipov/topuch/domain/document/PostDocument.java
+++ src/main/java/com/sharipov/topuch/domain/document/PostDocument.java
@@ -4,15 +4,93 @@ package com.sharipov.topuch.domain.document;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 
+import java.time.LocalDateTime;
+
 @Document(indexName = "products")
-public class ProductDocument {
+public class PostDocument {
 
     @Id
-    private String id;
+    private String postId;
 
-    private String name;
+    private String title;
     private String description;
     private Double price;
-    private String category;
-    private String brand;
+    private LocalDateTime createdAt;
+    private String introductionImageUrl;
+
+    private String condition;
+    private String categoryName;
+    private String subcategoryName;
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getIntroductionImageUrl() {
+        return introductionImageUrl;
+    }
+
+    public void setIntroductionImageUrl(String introductionImageUrl) {
+        this.introductionImageUrl = introductionImageUrl;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public String getSubcategoryName() {
+        return subcategoryName;
+    }
+
+    public void setSubcategoryName(String subcategoryName) {
+        this.subcategoryName = subcategoryName;
+    }
 }
