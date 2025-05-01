f6daeec45c25db6132e7eca3aa60caf5fd6fcbab
IslomW <119986336+IslomW@users.noreply.github.com>
2025-05-01 12:16:51 +0900

    feat(PostSearchServiceImpl): add service

--- src/main/java/com/sharipov/topuch/domain/service/impl/PostSearchServiceImpl.java
+++ src/main/java/com/sharipov/topuch/domain/service/impl/PostSearchServiceImpl.java
@@ -1,29 +1,112 @@
 package com.sharipov.topuch.domain.service.impl;
 
+import co.elastic.clients.elasticsearch._types.SortOptions;
+import co.elastic.clients.elasticsearch._types.SortOrder;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import com.sharipov.topuch.domain.document.PostDocument;
 import com.sharipov.topuch.domain.service.PostSearchService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.client.elc.NativeQuery;
+import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PostSearchServiceImpl implements PostSearchService {
+    private final ElasticsearchOperations elasticsearchOperations;
+
+    public PostSearchServiceImpl(ElasticsearchOperations elasticsearchOperations) {
+        this.elasticsearchOperations = elasticsearchOperations;
+    }
+
+//    private static final String INDEX = "posts";
+
+
+    //Поиск по ключевому слову
     @Override
     public List<PostDocument> searchPostsByKeyword(String keyword, int page, int size) {
-        return null;
+        Query matchQuery = Query.of(q -> q.match(
+                m -> m.field("content")
+                        .query(keyword)
+        ));
+
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(matchQuery)
+                .withPageable(PageRequest.of(page, size))
+                .build();
+
+        SearchHits<PostDocument> searchHits = elasticsearchOperations.search(query, PostDocument.class);
+        return searchHits.getSearchHits().stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public List<PostDocument> searchPostsByCategory(String categoryName, int page, int size) {
-        return null;
+        Query matchQuery = Query.of(q -> q.match(
+                m -> m.field("category")
+                        .query(categoryName)
+        ));
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(matchQuery)
+                .withPageable(PageRequest.of(page, size))
+                .build();
+
+        SearchHits<PostDocument> searchHits = elasticsearchOperations.search(query, PostDocument.class);
+        return searchHits.getSearchHits().stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
     }
 
     @Override
     public List<PostDocument> searchPostsByPriceRange(double minPrice, double maxPrice, int page, int size) {
-        return null;
+        Query rangeQuery = Query.of(q -> q.range(
+                rq -> rq.number(
+                        nrq -> nrq.field("price")
+                                .from(minPrice)
+                                .to(maxPrice)
+                )
+        ));
+
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(rangeQuery)
+                .withPageable(PageRequest.of(page, size))
+                .build();
+
+        SearchHits<PostDocument> searchHits = elasticsearchOperations.search(query, PostDocument.class);
+        return searchHits.getSearchHits().stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
+
     }
 
     @Override
     public List<PostDocument> searchAndSortByPrice(String keyword, boolean ascending, int page, int size) {
-        return null;
+        Query matchQuery = Query.of(q -> q.match(
+                m -> m.field("content")
+                        .query(keyword)
+        ));
+
+        SortOptions sortOptions = SortOptions.of(s -> s
+                .field(f -> f
+                        .field("price")
+                        .order(ascending ? SortOrder.Asc : SortOrder.Desc)
+                )
+        );
+
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(matchQuery)
+                .withSort(sortOptions)
+                .withPageable(PageRequest.of(page, size))
+                .build();
+
+        SearchHits<PostDocument> searchHits = elasticsearchOperations.search(query, PostDocument.class);
+        return searchHits.getSearchHits().stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -33,21 +116,59 @@ public class PostSearchServiceImpl implements PostSearchService {
 
     @Override
     public List<PostDocument> getAllPosts(int page, int size) {
-        return null;
+
+        Query matchAllQuery = Query.of(q -> q
+                .matchAll(ma -> ma));
+
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(matchAllQuery)
+                .withPageable(PageRequest.of(page, size))
+                .build();
+
+        SearchHits<PostDocument> searchHits = elasticsearchOperations.search(query, PostDocument.class);
+        return searchHits.getSearchHits().stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
     }
 
     @Override
     public long countPostsByKeyword(String keyword) {
-        return 0;
+        Query matchQuery = Query.of(q -> q
+                .match(m -> m
+                        .field("content")
+                        .query(keyword)
+                )
+        );
+
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(matchQuery)
+                .build();
+
+        return elasticsearchOperations.count(query, PostDocument.class);
     }
 
     @Override
     public void deletePostById(String postId) {
-
+        elasticsearchOperations.delete(postId, PostDocument.class);
     }
 
     @Override
     public List<PostDocument> searchPostsBySubcategory(String subcategoryName, int page, int size) {
-        return null;
+        Query termQuery = Query.of(q -> q
+                .term(t -> t
+                        .field("subcategory")
+                        .value(subcategoryName)
+                )
+        );
+
+        NativeQuery query = NativeQuery.builder()
+                .withQuery(termQuery)
+                .withPageable(PageRequest.of(page, size))
+                .build();
+
+        SearchHits<PostDocument> searchHits = elasticsearchOperations.search(query, PostDocument.class);
+        return searchHits.getSearchHits().stream()
+                .map(SearchHit::getContent)
+                .collect(Collectors.toList());
     }
 }
